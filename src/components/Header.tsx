4af7ed03db86d557b2924a5dda3a6f70c7bd1b91
sub0ne1 <iamsubhajit.d@gmail.com>
2023-08-21 15:12:43 -0400

    cleanup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,51 +6,29 @@ import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
 const Header = () => {
   return (
     <nav className='px-2 sm:px-4  w-full z-20 top-0 left-0 flex items-center justify-between py-10'>
-    <div className='container flex flex-wrap items-center justify-between mx-auto'>
-      <a href="/" className='flex items-center'> 
-        <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>Openjobs</span>
-      </a>
-      <nav className="flex gap-6">
-        <a
-          href='https://twitter.com/Ibelick'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='inline-flex'
+      <div className='container flex flex-wrap items-center justify-between mx-auto'>
+        <a href="/" className='flex items-center'> 
+          <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>Openjobs</span>
+        </a>
+        <nav className="flex gap-6">
+          <a
+            href='https://twitter.com/heysubha'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='inline-flex'>
+            <TwitterLogoIcon className='h-6 w-6 text-slate-400 transition-colors hover:text-white' />
+          </a>
+          <a
+            href='https://github.com/0verread/openjobs'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='inline-flex'
           >
-          <TwitterLogoIcon className='h-6 w-6 text-slate-400 transition-colors hover:text-white' />
-        </a>
-        <a
-          href='https://github.com/0verread/openjobs'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='inline-flex'
-        >
-          <GitHubLogoIcon className='h-6 w-6 text-slate-400 transition-colors hover:text-white' />
-        </a>
-      </nav>
-    </div>
-  </nav>
-    // <header className='flex items-center justify-between py-8'>
-    //   <div className='text-xl text-white'>Openjobs</div>
-    //   <nav className='flex gap-6'>
-    //     <a
-    //       href='https://twitter.com/Ibelick'
-    //       target='_blank'
-    //       rel='noopener noreferrer'
-    //       className='inline-flex'
-    //     >
-    //       {/* <TwitterLogoIcon className='h-6 w-6 text-slate-400 transition-colors hover:text-white' /> */}
-    //     </a>
-    //     <a
-    //       href='https://github.com/ibelick/ui-snippets'
-    //       target='_blank'
-    //       rel='noopener noreferrer'
-    //       className='inline-flex'
-    //     >
-    //       {/* <GitHubLogoIcon className='h-6 w-6 text-slate-400 transition-colors hover:text-white' /> */}
-    //     </a>
-    //   </nav>
-    // </header>
+            <GitHubLogoIcon className='h-6 w-6 text-slate-400 transition-colors hover:text-white' />
+          </a>
+        </nav>
+      </div>
+    </nav>
   );
 };
 

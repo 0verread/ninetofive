db2491296d5d6ceb51455919715483345b630d28
sub0ne1 <iamsubhajit.d@gmail.com>
2023-08-22 22:10:50 -0400

    color changes to kinda red

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -9,10 +9,10 @@ const Landing = () => {
       <section>
       <div className='relative z-0 mx-auto max-w-3xl pb-24 pt-12 text-center'>
         <div className='absolute -top-4 -z-10 flex w-full justify-center'>
-          <div className='h-[310px] w-[310px] max-w-full animate-pulse-slow rounded-full bg-[#8678F9] opacity-20 blur-[100px]' />
+          <div className='h-[310px] w-[310px] max-w-full animate-pulse-slow rounded-full bg-[#de4d40] opacity-20 blur-[100px]' />
         </div>
         <div>
-          <h1 className='mb-8 bg-gradient-to-t from-[#6d6d6d] to-[#f4f4f4] bg-clip-text text-4xl text-transparent md:text-5xl'>
+          <h1 className='mb-8 bg-gradient-to-t from-[#6d6d6d] to-[#e32e2e] bg-clip-text text-4xl text-transparent md:text-5xl'>
             an open source, community led job board and hiring platform
           </h1>
         </div>

d839a4dc77557e3bd4561e02ce30969e9980b4c6
sub0ne1 <iamsubhajit.d@gmail.com>
2023-08-24 15:44:23 -0400

    minor bug fixed;

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -9,7 +11,9 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 );
